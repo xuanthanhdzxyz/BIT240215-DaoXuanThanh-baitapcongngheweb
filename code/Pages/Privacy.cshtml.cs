1b7d6ceea23d080d77e0ee37f3654a4a9372fa8f
xuân thành <xuanthanh210206@gmail.com>
2026-05-26 10:55:44 +0700

    Add files via upload

---
+++ code/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VnEgoNews.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
